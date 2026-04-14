d1aa820c647c7533f48fb5297e22974deb96ebea
Ciro Martinez <martinezmartinciro@fpllefia.com>
2026-04-14 17:16:44 +0000

    rec

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [equipos, setEquipos] = useState([])
 
   async function leerEquipos() {
-    const ruta = "http://localhost:3000/api/equipos"
+    const ruta = "https://apichampions-rc51.onrender.com/api/equipos"
     const equiposJson = await fetch(ruta)
     const equipos = await equiposJson.json()
     setEquipos(equipos.equipos)
