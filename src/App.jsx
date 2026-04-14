28eb8a9846497d18e252b6adc6adf13169232a03
Ciro Martinez <martinezmartinciro@fpllefia.com>
2026-04-14 17:05:29 +0000

    arnauchupa

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [equipos, setEquipos] = useState([])
+
+  async function leerEquipos() {
+    const ruta = "http://localhost:3000/api/equipos"
+    const equiposJson = await fetch(ruta)
+    const equipos = await equiposJson.json()
+    setEquipos(equipos.equipos)
+  }
+
+  useEffect(() => {
+    leerEquipos()
+  }, [])
+
+  return (
+    <div>
+      <h1>EQUIPOS</h1>
+      {equipos && equipos.map((equipo) => (
+        <p>{equipo.nombre}</p>
+      ))}
+    </div>
+  )
+}
+
+export default App
